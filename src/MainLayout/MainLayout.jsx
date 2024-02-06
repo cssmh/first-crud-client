32c2132c0905b0db02b0e44472e94d403ec54fd6
momen210 <touristmomen@gmail.com>
2024-02-06 16:12:06 +0600

    post data menthod complete and success

--- src/MainLayout/MainLayout.jsx
+++ src/MainLayout/MainLayout.jsx
@@ -1,9 +1,45 @@
 const MainLayout = () => {
-    return (
-        <div>
-            <h2 className="text-center text-3xl font-semibold">First Simple CRUD</h2>
-        </div>
-    );
+  const handleAddUser = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const name = form.name.value;
+    const email = form.email.value;
+    const getUser = { name, email };
+    // console.log(getUser);
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(getUser),
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+      });
+  };
+
+  return (
+    <div>
+      <h2 className="text-center text-3xl font-semibold mt-16">
+        First Simple CRUD
+      </h2>
+      <div className="p-4 text-center">
+        <form onSubmit={handleAddUser}>
+          <input
+            className="border-2 mb-1 pl-1"
+            type="text"
+            name="name"
+            id="1"
+          />
+          <br />
+          <input className="border-2 pl-1" type="email" name="email" id="2" />
+          <br />
+          <input className="btn" type="submit" value="Add User" />
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default MainLayout;
