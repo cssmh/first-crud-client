4cff8b9fd14dc82d1458c558ba48d0cb9eec79bc
momen210 <touristmomen@gmail.com>
2024-02-06 15:37:58 +0600

    setup completed

---
+++ src/MainLayout/MainLayout.jsx
@@ -0,0 +1,9 @@
+const MainLayout = () => {
+    return (
+        <div>
+            <h2 className="text-center text-3xl font-semibold">First Simple CRUD</h2>
+        </div>
+    );
+};
+
+export default MainLayout;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import MainLayout from "./MainLayout/MainLayout";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout></MainLayout>,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <RouterProvider router={router} />
+  </React.StrictMode>
+);
